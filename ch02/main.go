2da7d34b3b6a369fe0734d224ccb09d077a5cff7
Msksgm <msksgm3124@gmail.com>
2022-01-04 18:46:01 +0900

    add: p.23

--- ch02/main.go
+++ ch02/main.go
@@ -1,26 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"os"
+)
 
 // インタフェースを定義
-type Talker interface {
-	Talk()
-}
-
-// 構造体を宣言
-type Greeter struct {
-	name string
-}
-
-// 構造体はTalkerインタフェースで定義されているメソッドを持っている
-func (g Greeter) Talk() {
-	fmt.Printf("Hello, my name is %s\n", g.name)
-}
-
 func main() {
-	// インタフェースの型を持つ変数を宣言
-	var talker Talker
-	// インタフェースを満たす構造体のポインタは代入できる
-	talker = &Greeter{"wozozo"}
-	talker.Talk()
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	file.Write([]byte("os.File example\n"))
+	file.Close()
 }
