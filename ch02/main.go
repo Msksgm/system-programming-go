b93096570b9cbbc8f6abc7fd46f4949b807e290b
Msksgm <msksgm3124@gmail.com>
2022-01-04 18:54:15 +0900

    add: p.25

--- ch02/main.go
+++ ch02/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"strings"
 )
 
 // インタフェースを定義
 func main() {
-	var buffer bytes.Buffer
-	buffer.Write([]byte("bytes.Buffer example\n"))
-	fmt.Println(buffer.String())
+	var builder strings.Builder
+	builder.Write([]byte("strings.Builder example\n"))
+	fmt.Println(builder.String())
 }
