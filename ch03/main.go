36c4373fffd19e0d11c669fff9f210d26b613155
Msksgm <msksgm3124@gmail.com>
2022-01-07 07:47:59 +0900

    add: p.55 chap3.6.3

--- ch03/main.go
+++ ch03/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"strings"
 )
 
-var source = "123 1.234 1.064 test"
+var csvSource = `13101,"100","1000003","トウキョウト","チヨダク","ヒトツバシ（1チョウメ）","東京都","千代田区","一ツ橋（１丁目）",1,0,1,0,0,13101,"101","1000003","トウキョウト","チヨダク","ヒトツバシ（2チョウメ）","東京都","千代田区","一ツ橋（２丁目）",1,0,1,0,0,13101,"100","1000012","トウキョウト","チヨダク","ヒトツバシ（2チョウメ）","東京都","千代田区","日比谷公園",0,0,0,0,0`
 
 func main() {
-	reader := strings.NewReader(source)
-	var i int
-	var f, g float64
-	var s string
-	fmt.Fscan(reader, &i, &f, &g, &s)
-	fmt.Printf("i=%#v f=%#v g=%#v s=%#v\n", i, f, g, s)
+	reader := strings.NewReader(csvSource)
+	csvReader := csv.NewReader(reader)
+	for {
+		line, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(line[2], line[6:9])
+	}
 }
