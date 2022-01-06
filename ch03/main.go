45b0d1804565dbfd8da8e8046c55e4f8ba1af3f5
Msksgm <msksgm3124@gmail.com>
2022-01-07 07:52:42 +0900

    add: p.56 chap3.7 io.MultiReader

--- ch03/main.go
+++ ch03/main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
+	"bytes"
 	"io"
-	"strings"
+	"os"
 )
 
-var csvSource = `13101,"100","1000003","トウキョウト","チヨダク","ヒトツバシ（1チョウメ）","東京都","千代田区","一ツ橋（１丁目）",1,0,1,0,0,13101,"101","1000003","トウキョウト","チヨダク","ヒトツバシ（2チョウメ）","東京都","千代田区","一ツ橋（２丁目）",1,0,1,0,0,13101,"100","1000012","トウキョウト","チヨダク","ヒトツバシ（2チョウメ）","東京都","千代田区","日比谷公園",0,0,0,0,0`
-
 func main() {
-	reader := strings.NewReader(csvSource)
-	csvReader := csv.NewReader(reader)
-	for {
-		line, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		fmt.Println(line[2], line[6:9])
-	}
+	header := bytes.NewBufferString("---- HEADER ----\n")
+	content := bytes.NewBufferString("Example of io.MultiReader\n")
+	footer := bytes.NewBufferString("---- FOOTER ----\n")
+
+	reader := io.MultiReader(header, content, footer)
+	// すべてのreaderをつなげた出力が表示
+	io.Copy(os.Stdout, reader)
 }
